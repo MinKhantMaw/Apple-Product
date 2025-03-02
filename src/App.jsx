8291bdb6f3263e72f93f791551c8286c72044b00
mkhant <minkhantmaw@onenex.co>
2025-03-02 21:10:41 +0630

    complete navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Controls from "./components/Controls";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  const [frameZoom, setFrameZoom] = useState(false);
+  const [activePage, setActivePage] = useState(0);
+  const handleNavClick = (pageIndex) => {
+    setActivePage(pageIndex);
+  };
+
+  const toggleZoom = () => {
+    setFrameZoom(!frameZoom);
+  };
+  return (
+    <div className="w-full h-screen grid place-items-center">
+      <div
+        className={`${
+          frameZoom && "min-w-[97vw] min-h-[97vh]"
+        } w-[70vw] h-[85vh] min-w[70vw] min-h[85vh] max-w[90vw] max-h[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100`}
+      >
+        <Navbar activePage={activePage} handleNavClick={handleNavClick} />
+        <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
