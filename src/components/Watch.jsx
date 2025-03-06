d147f6d09537e4f3ae79bf56c056dacff4af24ae
mkhant <minkhantmaw@onenex.co>
2025-03-06 23:02:58 +0630

    added: watch page

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { watchModels } from "../data/index";
 
 const Watch = () => {
-  return <div>Watch</div>;
+  return (
+    <div className="flex flex-col items-center justify-around bg-white h-full py-8 px-4">
+      <h1 className="2xl:text-4xl xl:text-3xl md:text-2xl font-semibold text-gray-800 mb-8">
+        Which Apple Watch is right for you?
+      </h1>
+      <div className="w-full flex justify-around">
+        {watchModels.map((watch, index) => (
+          <div className="w-60 h-[430px] flex flex-col justify-around group">
+            <div
+              key={index}
+              className="w-full h-64 mb-4 overflow-hidden relative"
+            >
+              <img
+                src={watch.imgs[0]}
+                alt="Watch"
+                className="absolute w-full h-full object-contain opacity-100 group-hover:opacity-0 transittion duration-300"
+              />
+              <img
+                src={watch.imgs[1]}
+                alt="Watch"
+                className="absolute w-full h-full object-contain opacity-0 group-hover:opacity-100 transittion duration-300"
+              />
+            </div>
+
+            <h3 className="2xl:text-xl xl:text-lg text-sm font-semibold">
+              {watch.name}
+            </h3>
+            <a href=" " className="my-2 text-base text-blue-400">
+              {watch.price}
+            </a>
+            <div className="md:text-sm text-xs text-gray-700 my-2">
+              {watch.desc.map((desc, i) => (
+                <span key={i} className="block">
+                  {desc}
+                </span>
+              ))}
+            </div>
+            <button
+              type="button"
+              className="w-min text-sm mt-4 py-1 px-2 rounded-full bg-blue-400 text-white"
+            >
+              Shop
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Watch;
